5ec930ffd7450aa291b95c8fdde6fcabe79a34e5
Mattish <Mattish@tentaclenoises.co.uk>
2013-08-05 17:11:17 +0100

    how do i git

--- Entities/Thing.cs
+++ Entities/Thing.cs
@@ -5,36 +5,32 @@ using Sce.PlayStation.Core;
 
 namespace Railtype_PSM_Engine.Entities{
 	public class Thing{
-		
+
 		float scalex,scaley,scalez,x,y,z,rotx,roty,rotz;
 		float[] modelVertex;
 		public int VertexCount;
 		public Matrix4 modelToWorld;
 		int number;
-		float var1;
-			
+
 		public Thing (){
-			VertexCount = 0;
 			modelToWorld = Matrix4.Identity;
-			modelToWorld.RowW = modelToWorld.RowW.Add(new Vector4(0.0f,0.0f,-1.0f,0.0f));
+			scalex = scaley = scalez = 1.0f;
+			x = y = z = rotx = roty = rotz = 0.0f;
 		}
-		
-		public Thing(float[] model){
+
+		public Thing(float[] model) : this(){
 			modelVertex = model;
-			VertexCount = model.Length;
-			modelToWorld = Matrix4.Identity;
-			modelToWorld.RowW = modelToWorld.RowW.Add(new Vector4(0.0f,0.0f,-1.0f,0.0f));
+			VertexCount = model.Length/3;
 		}
-		
-		public Thing(float[] model, int input){
+
+		public Thing(float[] model, int input) : this(){
+			number = input;
 			modelVertex = model;
-			VertexCount = model.Length;
+			VertexCount = model.Length/3;
 			modelToWorld = Matrix4.Identity;
-			modelToWorld.RowW = modelToWorld.RowW.Add(new Vector4(0.2f*input,0.0f,-2.0f,0.0f));
-			var1 = 0.0f;
 		}
-		
-		public Thing(int amountOfVertex, int input){
+
+		public Thing(int amountOfVertex, int input) : this(){
 			modelVertex = new float[amountOfVertex*3];
 			VertexCount = amountOfVertex;
 			number = input;
@@ -44,18 +40,31 @@ namespace Railtype_PSM_Engine.Entities{
 				modelVertex[i+2] = (float)Globals.random.NextDouble();
 			}
 		}
-		
+
 		public void update(){
-			var1 += 0.005f;
 			modelToWorld = Matrix4.Identity;
-			modelToWorld.RowW = modelToWorld.RowW.Add(new Vector4(0.2f,0.0f,-2.0f,0.0f));
-			modelToWorld *= Matrix4.RotationZ(var1);
+			modelToWorld *= Matrix4.RotationXyz(rotx,roty,rotz);
+			//modelToWorld.ColumnW = modelToWorld.ColumnW.Add(new Vector4(0.05f*number,0.0f,-5.0f,0.0f));
+			modelToWorld.ColumnW = new Vector4(x,y,z,modelToWorld.ColumnW.W);
+			modelToWorld *= Matrix4.Scale(scalex,scaley,scalez);
+			roty += 0.005f;
 		}
-		
+
 		public void PutModelVertexIntoArray(ref float[] input, int position){
-			Array.Copy(modelVertex,0,input,position,modelVertex.Length); 	
+			Vector4 tmpv = new Vector4(0,0,0,1); // Working out positions on CPU rather then passing matrix
+			Vector4 result = new Vector4(0,0,0,1);
+			for(int i = 0; i < modelVertex.Length; i+=3 ){
+				tmpv.X = modelVertex[i];
+				tmpv.Y = modelVertex[i+1];
+				tmpv.Z = modelVertex[i+2];
+				result = modelToWorld.Transform(tmpv);
+				input[position+i] = result.X;
+				input[position+i+1] = result.Y;
+				input[position+i+2] = result.Z;
+			}
+
+			//Array.Copy(modelVertex,0,input,position,modelVertex.Length); 	Old method
 		}
-		
+
 	}
 }
-
--- Globals.cs
+++ Globals.cs
@@ -2,76 +2,90 @@ using System;
 using System.Collections.Generic;
 using Sce.PlayStation.Core.Graphics;
 using Sce.PlayStation.Core;
+using System.Threading;
 using Railtype_PSM_Engine.Entities;
 
 namespace Railtype_PSM_Engine{
 	public static class Globals{
-		
-		public static int AmountToPush = 10;
+
+		public static int AmountPerPush = 10;
+
 		public static List<Thing> things;
 		public static int vertexSize;
-		public static Random random;
 		public static VertexBuffer modelVertexBuffer;
 		static private GraphicsContext _graphic;
+		public static Matrix4 ViewProjection;
+		public static Random random;
 		static public void Setup(GraphicsContext graphic){
 			_graphic = graphic;
 			random = new Random();
-			
 			things = new List<Thing>(100);
-			for(int i = 0; i < 100;i++){
-				things.Add(new Thing(250,i));
-			}
-		}
-		
-		static public void DoDrawing(ref GraphicsContext gc, ref ShaderProgram shaderProgram){
-			Primitive[] primArray;
-			int primCounter = 0, HowManyToPush = 0;
-			Matrix4[] matrixArray = PackModelVertexBuffer(ref gc, ref shaderProgram, out primArray);
-			         
 
-			while (primCounter < primArray.Length){
-				HowManyToPush = primArray.Length - primCounter > 10 ? 10 : primArray.Length - primCounter;
-				int k = shaderProgram.FindUniform("modelToWorld");
-				shaderProgram.SetUniformValue(k,matrixArray,0,primCounter,HowManyToPush);			
-				gc.DrawArrays(primArray,primCounter,HowManyToPush);
-				primCounter += HowManyToPush;
+			Vector3[] points = new Vector3[]{new Vector3(1,1,1),
+					new Vector3(1,1,-1),
+					new Vector3(1,-1,1),
+					new Vector3(1,-1,-1),
+					new Vector3(-1,1,1),
+					new Vector3(-1,1,-1),
+					new Vector3(-1,-1,1),
+					new Vector3(-1,-1,-1)};
+
+			int[] verticies = new int[]{0,1,5,0,5,4,
+										0,1,3,0,3,2,
+										1,5,7,1,7,3,
+										2,3,7,2,7,6,
+										0,4,6,0,6,2,
+										4,5,7,4,7,6};
+			float[] cubevertex = new float[108];
+			for(int i = 0; i < verticies.Length;i++){
+				cubevertex[(i*3)] = points[verticies[i]].X;
+				cubevertex[(i*3)+1] = points[verticies[i]].Y;
+				cubevertex[(i*3)+2] = points[verticies[i]].Z;
 			}
-			modelVertexBuffer.Dispose();
-			
+
+
+			for(int i = 0; i < 1;i++){
+				things.Add(new Thing(cubevertex,i));
+			}
 		}
-		
+
+		static public void DoDrawing(ref GraphicsContext gc, ref ShaderProgram shaderProgram){
+			System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+			Primitive[] primArray;
+			Matrix4[] matriciesToPush = PackModelVertexBuffer(ref gc, ref shaderProgram, out primArray);
+			sw.Start();
+
+			gc.DrawArrays(primArray,0,primArray.Length);
+			modelVertexBuffer.Dispose();
+
+			sw.Stop();
+			//Console.WriteLine("Draw/dispose loop took:" + sw.ElapsedMilliseconds + "ms");
+		}
+
 		static Matrix4[] PackModelVertexBuffer(ref GraphicsContext gc, ref ShaderProgram shaderProgram, out Primitive[] input){
-			input = new Primitive[things.Count];
 			float[] vertex;
 			Matrix4[] matrixUniform = new Matrix4[things.Count];
-			float[] matrixNumber;
-			int amountOfVertex = 0, matrixCounter = 0, position = 0;
+			input = new Primitive[things.Count];
+			int amountOfVertex = 0, matrixCounter = 0, position = 0, HowManyWeGot = 0;
 			for(int i = 0; i < things.Count;i++){ 
 				amountOfVertex += things[i].VertexCount;
 				matrixUniform[i] = things[i].modelToWorld;
+				HowManyWeGot++;
 			}
-			
+
 			vertex = new float[amountOfVertex*3];
-			matrixNumber = new float[amountOfVertex];
 			for(int i = 0; i < things.Count;i++){ // Now for each, put them into the arrays
 				things[i].PutModelVertexIntoArray(ref vertex,(position*3)); // For verticies
-				
-				for(int j = position; j < (position+things[i].VertexCount);j++){ // for matrixnumber
-					matrixNumber[j] = matrixCounter % AmountToPush;
-				}
-				
+
 				matrixCounter++;
 				input[i] = new Primitive(DrawMode.Triangles,position,things[i].VertexCount,0);
 				position += (things[i].VertexCount);
 			}			
-			
-            modelVertexBuffer = new VertexBuffer(amountOfVertex, VertexFormat.Float3,VertexFormat.Float);
+            modelVertexBuffer = new VertexBuffer(amountOfVertex, VertexFormat.Float3);
             modelVertexBuffer.SetVertices(0, vertex);
-			modelVertexBuffer.SetVertices(1, matrixNumber);
             gc.SetVertexBuffer(0, modelVertexBuffer);
 			return matrixUniform;
 		}
-		
+
 	}
 }
-
